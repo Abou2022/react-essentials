febd9d9a3cb1a302322365cfd5ace3a7a6720132
Bakary Sylla <syllabakary2002@gmail.com>
2022-06-21 06:03:29 -0700

    All test pass

---
+++ src/App.test.js
@@ -0,0 +1,18 @@
+import {render} from '@testing-library/react';
+//import jest dom
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+import App from './App';
+
+// test('renders an h1', () => {
+//     const {getByText} = render(<App/>);
+//     const h1 = getByText(/Hello React Testing Library/);
+//     expect(h1).toHaveTextContent('Hello React Testing Library');
+// });
+test("renders an h1", () => {
+    const { getByText } = render(<App />);
+    const h1 = getByText(/Hello React Testing Library/);
+    expect(h1).toHaveTextContent(
+         "Hello React Testing Library"
+    );
+});
