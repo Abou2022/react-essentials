dd2508231eca136c815c88ea63917fc9e5060cf9
Bakary Sylla <syllabakary2002@gmail.com>
2022-06-21 03:41:39 -0700

    handling loading states

--- src/App.js
+++ src/App.js
@@ -9,12 +9,24 @@ import './App.css';
 function App({login}) {
   //Need to create data variable 
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
 
   useEffect(()=> {
-    fetch(`https://api.github.com/users/${login}`)
+    if(!login) return;
+    setLoading(true)
+    fetch(`https://api.github.com/usrs/${login}`)
   .then((response) => response.json())
-  .then(setData);
-  }, []);
+  .then(setData)
+  .then (() =>  setLoading(false))
+  .catch(setError);
+  }, [login]);
+
+  if (loading) return <h1>Loading...</h1>
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>
+  if (!data) return null;
+
 
   // to fectch all data 
   // if(data){
@@ -22,21 +34,20 @@ function App({login}) {
   // }
 
   //to fectch specifique information (data)
-  if(data){
+  // if(data){
     return (<div>
       <h1>{data.name}</h1>
       <h3>{data.location}</h3>
-      <h3>{data.node_id}</h3>
       <img alt={data.login} src={data.avatar_url}/>
     </div>);
-  }
+  // }
 
 
-  return(
-   <div>
-    No user available
-   </div>
-  );
+  // return(
+  //  <div>
+  //   No user available
+  //  </div>
+  // );
 }
 
 export default App;
