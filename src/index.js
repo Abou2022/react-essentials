f1dd6020c9479dcac21699bc9619124d12b84c0d
Bakary Sylla <syllabakary2002@gmail.com>
2022-06-23 01:24:13 -0700

    add the routes file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App'
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 
 //important app function 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<App login='Abou2022'/>
+    <Router>
+        <App login='Abou2022'/>
+    </Router>
+
 
 );
