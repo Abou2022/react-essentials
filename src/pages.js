1a4ac4541aad891a417a857cb91861b9f0b3477f
Bakary Sylla <syllabakary2002@gmail.com>
2022-06-23 01:41:50 -0700

    add link component to open the link

--- src/pages.js
+++ src/pages.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export function Home(){
     return(
         <div>
             <h1>[Comapany Website]</h1>
+            <nav>
+                <Link to='about'>About</Link>
+                <Link to='events'>Events</Link>
+                <Link to='contact'>Contact</Link>
+            </nav>
         </div>
     )
 }
@@ -11,6 +17,9 @@ export function About(){
     return(
         <div>
             <h1>[About]</h1>
+            <nav>
+                <Link to='/'>Home</Link>
+            </nav>
         </div>
     )
 }
@@ -18,6 +27,9 @@ export function Events(){
     return(
         <div>
             <h1>[Events]</h1>
+            <nav>
+                <Link to='/'>Home</Link>
+            </nav>
         </div>
     )
 }
@@ -25,6 +37,24 @@ export function Contact(){
     return(
         <div>
             <h1>[Contact]</h1>
+            <nav>
+                <Link to='/'>Home</Link>
+            </nav>
+        </div>
+    )
+}
+
+export function Whoops404(){
+
+    let location = useLocation();
+    console.log(location)
+    return(
+        <div>
+            {/* <h1>This page does not exist!!</h1> */}
+            <h1>Resource not found at {location.pathname}</h1>
+            <nav>
+                <Link to='/'>Home</Link>
+            </nav>
         </div>
     )
 }
